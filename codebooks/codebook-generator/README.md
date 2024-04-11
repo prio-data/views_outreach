d6dd2399cfb2a045c187059b150a313785b67d8f
Sara Kallis <sarakallis@protonmail.com>
2024-04-11 17:42:43 +0200

    Refactor and add README

---
+++ codebooks/codebook-generator/README.md
@@ -0,0 +1,26 @@
+# Codebook Generator
+
+## Overview
+
+The Codebook Generator is designed to generate Markdown codebooks from JSON data. It can fetch data from either an API or a local file, convert it to Markdown format, and save the Markdown codebook to a specified directory.
+
+## Prerequisites
+- Access to ViEWS Dropbox
+
+## Configuration
+
+Before running the script `[codebook_generator_json_to_md.py](codebook_generator__json_to_md.py)`, you need to configure the `[codebook_generator__config.py](codebook_generator__config.py)` file.
+
+### Configurable Variables
+
+1. `outcome_type`: Specify the type of outcome data ("forecasts" or "predictors"), i.e., what data is the codebook documenting?
+2. `fetch_type`: Specify the method to fetch the codebook data ("api" or "file").
+3. `run`: Specify the run value for the API endpoint. Also used in the naming of the final file.
+
+
+## How to run this script
+After making necessary changes to `[codebook_generator__config.py](codebook_generator__config.py)`, run the script by clicking on the Run button in the top right of your IDE, or executing the following in Terminal:
+
+```bash
+python codebook_generator__json_to_md.py
+```
--- codebooks/codebook-generator/codebook_generator__config.py
+++ codebooks/codebook-generator/codebook_generator__config.py
@@ -1,29 +1,15 @@
-"""
-This file contains all parameters required to instruct the 
-markdown (.md) codebook generator on:
+from pathlib import Path
 
-- What source JSON codebook to use (API or local file)
-- What type of output it will generate (codebook for forecasts or predictor output)
-- What run the codebook concerns (for forecasts only)
+#CHANGE:
+outcome_type = "predictors" # "predictors" or "fatalities"
+fetch_type = "api"  # "api" or "file"
+run = "fatalities002_2024_02_t01" 
+save_path = Path.home() / "ViEWS Dropbox" / "ViEWS" / "Codebooks" / "PDFs"
 
-Make your changes below, save, and then run the Codebook Generator script to produce the .md material.
-"""
-
-# Are you creating a codebook for forecast data or predictor data? 
-# Choose "predictors" or "forecasts"
-outcome_type = "predictors" 
-
-# How are you loading the JSON version of the codebook?
-# Choose "api" or "file"
-fetch_type = "api" # api or file
-
-# Where do you want to save your PDF file? Default is the VIEWS Dropbox under Codebooks/PDFs. 
-home = os.path.expanduser("~") # Don't change this line
-save_path = home + "/ViEWS Dropbox/ViEWS/Codebooks/PDFs/" # Edit here
-
-# If you are generating a codebook for PREDICTOR data, you are 
-# done here. No more edits needed. 
-
-# If you are generating a codebook for FORECAST data, enter the run ID below:
-run = "fatalities002_2024_02_t01"
+#DON'T CHANGE
+# Logic to set the value of run based on outcome_type
+if outcome_type == "predictors":
+    run = "predictors_fatalities002_0000_00"
+else:
+    run = run #what you defined above
 
